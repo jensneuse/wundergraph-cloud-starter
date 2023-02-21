0a7e735012dca151126e32b06e759ffa5ce2dc71
Jens Neuse <jens.neuse@gmx.de>
2023-02-21 17:17:09 +0100

    +hello

--- .wundergraph/operations/hello.ts
+++ .wundergraph/operations/hello.ts
@@ -2,7 +2,7 @@ import {createOperation} from "../generated/wundergraph.factory";
 
 export default createOperation.query({
     handler: async (ctx) => {
-        console.log("Hello, World!")
+        console.log("hello")
         return ctx.internalClient.queries.Dragons();
     }
 })
